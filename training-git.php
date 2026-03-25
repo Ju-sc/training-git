b7c889d5f820de9c50bb1b48baa344a8b6c7c3f1
jull-hash <dacruznetojulio@gmail.com>
2026-03-25 20:37:01 -0300

    inicio da estrutura do jogo

--- training-git.php
+++ training-git.php
@@ -1,6 +1,12 @@
 <?php
 
+session_start();
 
+$numerodojogador = $_POST["numero"] ?? NULL;
+
+if (!empty($numerodojogador)){
+    $randomNumber = rand(1, 10);
+}
 
 ?>
 
@@ -15,6 +21,28 @@
 
 
 </head>
+
+<body>
+    <form method="post" action="training-git.php">
+    <div class="mb-4">
+                <label for="numero" class="block text-gray-700 font-medium mb-2">
+                numero
+                </label>
+                <input
+                    type="numero"
+                    name="numero"
+                    required
+                    placeholder="Digite seu numero"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+            </div>
+    
+    <?php if (!empty($numerodojogador)): ?>
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+                <?php echo $randomNumber; ?>
+        </div> 
+    <?php endif; ?>
+</body>
 
 
 
