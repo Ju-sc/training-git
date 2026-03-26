e9ecdce61b60f74537cf5426e730432e50e5a63b
jull-hash <dacruznetojulio@gmail.com>
2026-03-25 22:04:14 -0300

    botão aprimorado e ideia de animação

--- training-git.php
+++ training-git.php
@@ -2,13 +2,16 @@
 
 session_start();
 
+$jogou = FALSE;
+$jogada = "";
 
 if (isset($_GET["jogou"])){
+    $jogou = TRUE;
     $randomNumber = rand(1, 2);
     if ($randomNumber == 1){
-        echo ("coroa");
+        $jogada = "coroa";
     } else {
-        echo ("cara");
+        $jogada = "cara";
     }
 }
 
@@ -25,17 +28,42 @@ if (isset($_GET["jogou"])){
     <!--- Tailwind CSS via CDN --->
     <script src="https://cdn.tailwindcss.com"></script>
 
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+    /* The animation code */
+    @keyframes myAnimation {
+    from {background-color: black;}
+    to {background-color: white;}
+    }
+
+    /* The element to apply the animation to */
+    div {
+    width: 100px;
+    height: 50px;
+    animation-name: myAnimation;
+    animation-duration: 1s; /* Required property for the animation to run */
+
+    animation-direction: alternate; /* Makes it reverse direction each time */
+    }
+</style>
 
 </head>
 
 <body>
+    <?php if (isset($_GET["jogou"])): ?>
+    <div class="div  border border-black-900 px-4 py-3 rounded mb-6">
+                <?php echo $jogada; ?>
+    </div>
+    <?php endif; ?>
+   
+
     <form method="post" action="training-git.php">
                 
                       
             <a href="training-git.php?jogou" 
-            <button onclick="return confirm('Jogar?')"
-            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"> 
-                            Clique aqui 
+            type="submit"
+                                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                                <?= $jogou ? "jogar novamente" : "jogar" ?>
                             </button>
                             </a>
            
